d9d12a224bf141b8aff83454259cac0b57eb82b4
confidenseuide <confidenseuide@gmail.com>
2026-01-03 16:43:21 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends Activity {
             startForegroundService(intent);
         } else {
            startService(intent);} 
-        finishAndRemoveTask(); 
         return;}
 
         
