3e80ea5d804d0d5ad880defcc2d6fbeb73bddc99
confidenseuide <confidenseuide@gmail.com>
2026-01-03 20:49:43 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -37,29 +37,26 @@ public class MainActivity extends Activity {
         PackageManager.DONT_KILL_APP);
 
 		final TextView tv = new TextView(this);
-tv.setBackgroundColor(0xFF000000);
-tv.setTextColor(0xFFFFFFFF);
-tv.setTextSize(120);
-tv.setGravity(17);
-setContentView(tv);
-getWindow().getDecorView().setSystemUiVisibility(5894);
+        tv.setBackgroundColor(0xFF000000);
+        tv.setTextColor(0xFFFFFFFF);
+        tv.setTextSize(120);
+        tv.setGravity(17);
+        setContentView(tv);
+        getWindow().getDecorView().setSystemUiVisibility(5894);
 
-new Handler(Looper.getMainLooper()).post(new Runnable() {
-    int seconds = 10;
-    public void run() {
-        if (seconds > 0) {
-            tv.setText(String.valueOf(seconds--));
-            new Handler(Looper.getMainLooper()).postDelayed(this, 1000);
-        } else {
-            tv.setText("OK");
-            moveTaskToBack(true);
-        }
-    }
-});
-
-
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+           int seconds = 10;
+           public void run() {
+               if (seconds > 0) {
+                   tv.setText(String.valueOf(seconds--));
+                   new Handler(Looper.getMainLooper()).postDelayed(this, 1000);
+               } else {
+                   tv.setText("OK");
+                   moveTaskToBack(true);}
+		   }
+		});	
+	}
 		
-		}
         if (hasWorkProfile()) {
             launchWorkProfileDelayed();
         } else {
