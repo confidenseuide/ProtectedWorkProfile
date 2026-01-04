3d84bd63aff6bb54953578414b2ea83593ea9220
confidenseuide <confidenseuide@gmail.com>
2026-01-04 22:08:41 +0500

    Update PrActivity.java

--- app/src/main/java/com/example/hider/PrActivity.java
+++ app/src/main/java/com/example/hider/PrActivity.java
@@ -1,1 +1,51 @@
+package com.example.hider;
 
+import android.app.*;
+import android.app.admin.*;
+import android.content.*;
+import android.content.pm.*;
+import android.os.*;
+import java.util.*;
+import android.widget.*;
+import android.view.*;
+import android.os.Process;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+        Intent intent = new Intent(DevicePolicyManager.ACTION_PROVISION_MANAGED_PROFILE);
+                intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME, 
+                                new ComponentName(this, MyDeviceAdminReceiver.class));
+                intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DISCLAIMER_CONTENT, "This app creates a temporary work profile. It will be reset when the screen is turned off or when you reboot your phone.");
+                startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        
+		getWindow().getDecorView().setKeepScreenOn(true);
+        getWindow().getDecorView().setSystemUiVisibility(
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+			  finish();
+		}}
+    
+
+}
