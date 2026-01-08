2546a57c8450cde196637dfde3894cfa1622d8c8
confidenseuide <confidenseuide@gmail.com>
2026-01-09 04:32:11 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -130,13 +130,6 @@ public class MainActivity extends Activity {
         );
     }
 
-   // @Override
-   // protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-      //  super.onActivityResult(requestCode, resultCode, data);
-      //  if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
-		//	restart();
-	//	}
-   // }
 	@Override
 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     if (requestCode == 100) {
