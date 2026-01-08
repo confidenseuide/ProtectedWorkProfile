93b1c5bcae652f3aad270d007f23ab9d479a3f28
confidenseuide <confidenseuide@gmail.com>
2026-01-08 19:13:40 +0500

    Update SetPasswordActivity.java

--- app/src/main/java/com/example/hider/SetPasswordActivity.java
+++ app/src/main/java/com/example/hider/SetPasswordActivity.java
@@ -3,6 +3,7 @@ package com.example.hider;
 import android.app.*;
 import android.app.admin.*;
 import android.content.*;
+import android.content.pm.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
