9b3f7c27a3c3a717a36c34520d8582ecce40381e
confidenseuide <confidenseuide@gmail.com>
2026-01-08 21:56:08 +0500

    Update DestroyActivity.java

--- app/src/main/java/com/example/hider/DestroyActivity.java
+++ app/src/main/java/com/example/hider/DestroyActivity.java
@@ -1,4 +1,18 @@
 package com.example.hider;
 public class DestroyActivity extends Activity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+		dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);		
+		if (!isWorkProfileContext() && hasWorkProfile()) {
+            launchWorkProfileDelayed();
+		}
+		if (!isWorkProfileContext() && !hasWorkProfile()) {
+			Intent intent = new Intent(this, MainActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+			finish();
+    }
   
 }
