096e1a4db53f912ef6403a7c83f0d3e0f69c4824
confidenseuide <confidenseuide@proton.me>
2026-01-12 04:01:56 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -13,7 +13,7 @@ import android.os.Process;
 
 public class MainActivity extends Activity {
 
-	String a=""
+	String a="";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +63,7 @@ public class MainActivity extends Activity {
 							    try {
 								    java.lang.reflect.Method method = dpm.getClass().getMethod("setAdminExemptFromBackgroundRestrictedOperations", ComponentName.class, boolean.class);
 								    method.invoke(dpm, admin, true);
-									a="App excluded from batery restrictions for stable service and receivers work "
+									a="App excluded from battery restrictions for stable service and receivers work ";
 							    }catch (Exception ignored) {}
 							
 							}
