76aa79d1a09c24cbbe0c958f4d122d9e86e01222
confidenseuide <confidenseuide@proton.me>
2026-01-12 19:42:01 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -102,7 +102,10 @@ public class MainActivity extends Activity {
 
     btn.setOnClickListener(v -> {
         
-        // ТУТ ТВОЙ КОД
+		Intent intent = new Intent(DevicePolicyManager.ACTION_PROVISION_MANAGED_PROFILE);
+        intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME, new ComponentName(this, MyDeviceAdminReceiver.class));
+        intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DISCLAIMER_CONTENT, "This app creates a temporary work profile. It will be reset when the screen is turned off or when you reboot your phone.");
+        startActivityForResult(intent, 100);    
         
         dialog.dismiss();
     });
