56e9e612eac198916c51276596a2361ca9f014c9
confidenseuide <confidenseuide@proton.me>
2026-01-16 09:09:08 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -186,7 +186,7 @@ public class MainActivity extends Activity {
             "2. App tries to ignore battery optimization and disable package control to prevent stop-signals from system.\n" +
             "3. App adds \"safest\" system browser to profile (with less permissions from ours blacklist).\n" +
             "4. App disables screenshots in profile (safety), allows apps install and accounts management (user freedom).\n" +
-            "5. App selects \"safest\" system keyboard and freezes others.\n"
+            "5. App selects \"safest\" system keyboard and freezes others.\n"+
 		    "6. App asks you to set profile password to protect data\n"+
 			"7. When screen turns off or phone reboots profile will be freezed and profie apps hidden (except this app)\n"+
 			"8. To unhide apps just click to \"ProtectedWorkProfile\" shortcut and wait for the timer.\n\n");
