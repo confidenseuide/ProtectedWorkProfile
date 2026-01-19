acc901921e0398a16b8096c883bc70d9d3c30219
confidenseuide <confidenseuide@proton.me>
2026-01-19 20:00:48 +0500

    Update NucleusReceiver.java

--- app/src/main/java/protectedwp/safespace/NucleusReceiver.java
+++ app/src/main/java/protectedwp/safespace/NucleusReceiver.java
@@ -7,36 +7,6 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class NucleusReceiver extends BroadcastReceiver {
-    
-    private void setAppsVisibility(Context context, final boolean visible) {
-    final DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-    final ComponentName admin = new ComponentName(context, MyDeviceAdminReceiver.class);
-    final PackageManager pm = context.getPackageManager();
-
-    if (!dpm.isProfileOwnerApp(context.getPackageName())) return;
-
-    List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.MATCH_UNINSTALLED_PACKAGES);
-
-    for (ApplicationInfo app : packages) {
-        String pkg = app.packageName;
-
-        if (pkg.equals(context.getPackageName())) continue;
-
-        Intent launcherIntent = new Intent(Intent.ACTION_MAIN, null);
-        launcherIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        launcherIntent.setPackage(pkg);
-
-        List<ResolveInfo> activities = pm.queryIntentActivities(launcherIntent, 
-                PackageManager.MATCH_DISABLED_COMPONENTS | PackageManager.MATCH_UNINSTALLED_PACKAGES);
-
-        if (activities != null && !activities.isEmpty()) {
-            try {
-                dpm.setApplicationHidden(admin, pkg, !visible);
-            } catch (Exception ignored) {
-            }
-        }
-    }
-}
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -47,8 +17,6 @@ public class NucleusReceiver extends BroadcastReceiver {
             action.equals(Intent.ACTION_MANAGED_PROFILE_UNLOCKED)) {
 
             DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-            
-            setAppsVisibility(context, false); 
 
             Intent serviceIntent = new Intent(context, WatcherService.class);
             try {
