61fa5d1b1135b41f4f8294df084e25aa84c0f364
confidenseuide <confidenseuide@proton.me>
2026-01-20 15:03:43 +0500

    Update MainActivity.java

--- app/src/main/java/protectedwp/safespace/MainActivity.java
+++ app/src/main/java/protectedwp/safespace/MainActivity.java
@@ -185,8 +185,7 @@ public class MainActivity extends Activity {
 		    "6. App asks you to set profile password to protect data. It is also recommended to set a password for your main phone, not just your profile.\n"+
 			"7. When screen turns off profile will be frozen and profie apps hidden (except this app)\n"+
 			"8. To unhide apps just click to \"ProtectedWorkProfile\" shortcut and wait for the timer.\n\n" +
-			"This profile is designed to protect any data in everyday life.\nEven if you are an ordinary user — this app is for you.\nBut be careful: do not connect your phone to USB (PC, mouse, etc).\nAnd do not press the Destroy🔥 button if you do not want to delete data.\nThe Destroy🔥 button is needed to quickly delete data in an emergency situation, for example, when you are being forced to show your phone.\nYou can also enable extreme protection for such cases in AdditionalOptions.\nData wiping upon USB connection is needed for protection against exploits.\n\n");
-    
+			"This profile intended to protect any data in everyday life.\nEven if you ordinary user — this app is for you.\nBut be careful: don't connect phone to USB (PC, mouse, etc).\nAnd don't tap on Destroy🔥 button if you don't want delete data.\nDestroy🔥 button needed to quickly delete data in emergency situation, for example when you forced to show phone.\nYou can also enable extreme protection for such cases in AdditionalOptions.\nData wiping upon USB connection needed for protection against exploits.\n\n");
     scroll.addView(tv);
     root.addView(scroll, sParams);
 
