4a9e9dc0f2e7b808e2dbaa2989fd98a279f560cf
confidenseuide <confidenseuide@proton.me>
2026-01-22 18:37:12 +0500

    Update AdditionalOptionsActivity.java

--- app/src/main/java/protectedwp/safespace/AdditionalOptionsActivity.java
+++ app/src/main/java/protectedwp/safespace/AdditionalOptionsActivity.java
@@ -19,6 +19,16 @@ public class AdditionalOptionsActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+		getWindow().getDecorView().setKeepScreenOn(true);
+        getWindow().getDecorView().setSystemUiVisibility(
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
 		if (!isWorkProfileContext() && hasWorkProfile()) {
             launchWorkProfileDelayed();
 		}
