e060ff90cc78fe94602f7912fb5f27cae7ae33e0
confidenseuide <confidenseuide@proton.me>
2026-01-23 20:54:41 +0500

    Update SetPasswordActivity.java

--- app/src/main/java/protectedwp/safespace/SetPasswordActivity.java
+++ app/src/main/java/protectedwp/safespace/SetPasswordActivity.java
@@ -5,24 +5,96 @@ import android.app.admin.*;
 import android.content.*;
 import android.content.pm.*;
 import android.os.*;
-import java.util.*;
-import android.widget.*;
 import android.view.*;
-import android.view.inputmethod.*;
-import android.os.Process;
+import android.widget.*;
+import java.util.*;
 
 public class SetPasswordActivity extends Activity {
 
-  @Override
+	private DevicePolicyManager dpm;
+	
+    @Override
     protected void onResume() {
-        super.onResume();        
-         this.createDeviceProtectedStorageContext()
-            .getSharedPreferences("prefs", Context.MODE_PRIVATE)
-            .edit()
-            .putBoolean("isDone", false)
-            .commit();
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);        
-        startActivity(intent);
+        super.onResume();
+		dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);		
+		if (!isWorkProfileContext() && hasWorkProfile()) {
+            launchWorkProfileDelayed();
+		}
+		if (!isWorkProfileContext() && !hasWorkProfile()) {
+			Intent intent = new Intent(this, MainActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+			finish();
+		}
+		getWindow().getDecorView().setKeepScreenOn(true);
+        getWindow().getDecorView().setSystemUiVisibility(
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
     }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);		
+	
+		if (isWorkProfileContext() && hasWorkProfile()) {
+			
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setGravity(Gravity.CENTER);
+
+        Button btnChange = new Button(this);
+        btnChange.setText("Set Password");
+
+        btnChange.setOnClickListener(v -> {
+            Intent intent = new Intent(DevicePolicyManager.ACTION_SET_NEW_PASSWORD);
+            try {
+                startActivity(intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        layout.addView(btnChange);
+        setContentView(layout);
+    }}
+	private boolean isWorkProfileContext() {
+        DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        return dpm.isProfileOwnerApp(getPackageName());
+    }
+
+    private boolean hasWorkProfile() {
+        UserManager userManager = (UserManager) getSystemService(Context.USER_SERVICE);
+        return userManager.getUserProfiles().size() > 1;
+    }
+
+    private void launchWorkProfileDelayed() {
+    new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            LauncherApps launcherApps = (LauncherApps) getSystemService(Context.LAUNCHER_APPS_SERVICE);
+            UserManager userManager = (UserManager) getSystemService(Context.USER_SERVICE);
+            
+            if (launcherApps != null && userManager != null) {
+                List<UserHandle> profiles = userManager.getUserProfiles();
+                for (UserHandle profile : profiles) {
+                   if (userManager.getSerialNumberForUser(profile) != 0) {
+                        launcherApps.startMainActivity(
+                            new ComponentName(getPackageName(), SetPasswordActivity.class.getName()), 
+                            profile, null, null
+                        );
+                        
+                        finishAndRemoveTask();
+                        break;
+                    }
+                }
+            }
+        }
+    }, 1000);
+	}
 }
