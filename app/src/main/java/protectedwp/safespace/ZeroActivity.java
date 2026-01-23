2360570ade75f5027d4a74ee304d21c7b2f10ec3
confidenseuide <confidenseuide@proton.me>
2026-01-23 20:53:29 +0500

    Update ZeroActivity.java

--- app/src/main/java/protectedwp/safespace/ZeroActivity.java
+++ app/src/main/java/protectedwp/safespace/ZeroActivity.java
@@ -9,13 +9,17 @@ import java.util.*;
 import android.widget.*;
 import android.view.*;
 import android.view.inputmethod.*;
-import android.os.Process;
 
 public class ZeroActivity extends Activity {
 
   @Override
     protected void onResume() {
         super.onResume();        
+         this.createDeviceProtectedStorageContext()
+            .getSharedPreferences("prefs", Context.MODE_PRIVATE)
+            .edit()
+            .putBoolean("isDone", false)
+            .commit();
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);        
         startActivity(intent);
