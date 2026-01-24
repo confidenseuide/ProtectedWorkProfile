e4075242cd7d8bbfa47acbe77b80a0efbaadbbf7
confidenseuide <confidenseuide@proton.me>
2026-01-25 02:22:16 +0500

    Update MainActivity.java

--- app/src/main/java/protectedwp/safespace/MainActivity.java
+++ app/src/main/java/protectedwp/safespace/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity {
     layout.setPadding(padding, padding, padding, padding);
 
     android.widget.TextView tv = new android.widget.TextView(this);
-    tv.setText("Please set password for profile if you haven't done so yet. Use a password type that is different from the password type of the main profile.");
+    tv.setText("Please set password for profile to encrypt profile data if you haven't done so yet. Use a password type that is different from the password type of the main profile.");
     tv.setTextSize(18);
     tv.setTextColor(0xFF000000);
     tv.setPadding(0, 0, 0, padding);
