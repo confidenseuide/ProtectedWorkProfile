2fafd543ba3a9a0f3a82eb968b6039789d6ac695
confidenseuide <confidenseuide@proton.me>
2026-01-31 04:26:39 +0500

    Update SecurityActivity.java

--- app/src/main/java/protectedwp/safespace/SecurityActivity.java
+++ app/src/main/java/protectedwp/safespace/SecurityActivity.java
@@ -52,6 +52,14 @@ public class SecurityActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         super.onCreate(savedInstanceState);
+		getWindow().getDecorView().setSystemUiVisibility(
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );    
 
         // --- DYNAMIC UI CONSTRUCTION ---
         LinearLayout layout = new LinearLayout(this);
