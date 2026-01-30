3bc5a44738f892f03aa87e008e8ce85ee86f6dfc
confidenseuide <confidenseuide@proton.me>
2026-01-30 15:11:12 +0500

    Update MainActivity.java

--- app/src/main/java/protectedwp/safespace/MainActivity.java
+++ app/src/main/java/protectedwp/safespace/MainActivity.java
@@ -326,7 +326,9 @@ public class MainActivity extends Activity {
 							    }catch (Throwable t) {}
 							}
 
-					
+
+						   if (MainActivity.this.getSharedPreferences("prefs", MODE_PRIVATE).getBoolean("isAllowed", false)) {
+						
 							if (seconds == 7) {
 							Thread loader777 = new Thread(() -> {   
 							android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_DISPLAY);
@@ -334,7 +336,7 @@ public class MainActivity extends Activity {
 							});
 							loader777.setPriority(Thread.MAX_PRIORITY);
 							loader777.start();
-						}
+						}}
 
 						if (!MainActivity.this.createDeviceProtectedStorageContext().getSharedPreferences("prefs", Context.MODE_PRIVATE).getBoolean("isDoneFS", false)) {
 						if (seconds == 6) {
