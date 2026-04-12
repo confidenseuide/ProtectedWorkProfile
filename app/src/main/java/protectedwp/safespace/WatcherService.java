d8daaf5c1aef85f7b4bec6bd0d20a51e7ba777fe
confidenseuide <252536920+confidenseuide@users.noreply.github.com>
2026-04-12 15:02:52 +0500

    Update WatcherService.java

--- app/src/main/java/protectedwp/safespace/WatcherService.java
+++ app/src/main/java/protectedwp/safespace/WatcherService.java
@@ -45,7 +45,9 @@ public class WatcherService extends DeviceAdminService {
     if (needNew || activeId == null) {
         activeId = "protectedwp.safespace" + Long.toHexString(new java.security.SecureRandom().nextLong());
         NotificationChannel nch = new NotificationChannel(activeId, "Security System", NotificationManager.IMPORTANCE_DEFAULT);
-        nm.createNotificationChannel(nch);
+        nch.setSound(null, null);
+		nch.enableVibration(false);
+		nm.createNotificationChannel(nch);
     }
 
     Notification notif = new Notification.Builder(context, activeId)
