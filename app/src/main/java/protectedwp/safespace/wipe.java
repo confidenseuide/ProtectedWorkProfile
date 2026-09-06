eed12b5a59e9d20381810754e28994bec66d3ee5
confidenseuide <252536920+confidenseuide@users.noreply.github.com>
2026-09-06 18:22:36 +0500

    Update wipe.java

--- app/src/main/java/protectedwp/safespace/wipe.java
+++ app/src/main/java/protectedwp/safespace/wipe.java
@@ -1,5 +1,7 @@
 package protectedwp.safespace;
 
+import android.os.Build;
+import android.content.SharedPreferences;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
@@ -8,8 +10,37 @@ import android.os.UserHandle;
 
 public class wipe {
 
+    private static boolean isCopeOwner(Context c) {
+        DevicePolicyManager dpm = (DevicePolicyManager) c.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        boolean isCOPE = dpm != null && dpm.isOrganizationOwnedDeviceWithManagedProfile() && dpm.isProfileOwnerApp(c.getPackageName());
+        return isCOPE;
+    }
+
+    private static boolean shouldWipeAllPhoneData(Context c) {
+    Context deviceProtectedContext = c.createDeviceProtectedStorageContext();
+    SharedPreferences devicePrefs = deviceProtectedContext.getSharedPreferences("prefs", Context.MODE_PRIVATE);
+    return devicePrefs.getBoolean("wipe_all_phone_data", false);
+    }
+
     public static void wipe(Context context) {
         Context c = context.getApplicationContext();
+
+        if (shouldWipeAllPhoneData(c) && isCopeOwner(c)) {
+            ComponentName adminName = new ComponentName(c, MyDeviceAdminReceiver.class);
+            DevicePolicyManager dpm = (DevicePolicyManager) c.getSystemService(Context.DEVICE_POLICY_SERVICE);         
+            DevicePolicyManager parentDpm = dpm.getParentProfileInstance(adminName);
+          
+            int flags = DevicePolicyManager.WIPE_RESET_PROTECTION_DATA 
+              | DevicePolicyManager.WIPE_EUICC 
+              | DevicePolicyManager.WIPE_EXTERNAL_STORAGE;
+
+            if (Build.VERSION.SDK_INT >= 34) {    
+            dpm.wipeDevice(flags);
+            } else {
+                parentDpm.wipeData(flags);
+            }
+            return;
+        }
 
         try {
             ((DevicePolicyManager)c.getSystemService(Context.DEVICE_POLICY_SERVICE)).wipeData(DevicePolicyManager.WIPE_SILENTLY);
