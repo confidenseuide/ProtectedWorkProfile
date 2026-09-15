b9172fb709a2cfe096c39227150d69f27a5d0521
confidenseuide <252536920+confidenseuide@users.noreply.github.com>
2026-09-15 15:52:57 +0500

    Update DestroyActivity.java

--- app/src/main/java/protectedwp/safespace/DestroyActivity.java
+++ app/src/main/java/protectedwp/safespace/DestroyActivity.java
@@ -12,50 +12,10 @@ public class DestroyActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-		if (isWorkProfileContext()) {
-			wipe.wipe(this);	
-		}	
 		
-		if (!isWorkProfileContext() && hasWorkProfile()) {
-            launchWorkProfileDelayed();
-		}
-		
-		if (!isWorkProfileContext() && !hasWorkProfile()) {
-			Intent intent = new Intent(this, MainActivity.class);
-			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			startActivity(intent);
-			finish();
-		}
+		wipe.wipe(this);	
+			
 	}
-	
-	private boolean isWorkProfileContext() {
-        DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-        return dpm.isProfileOwnerApp(getPackageName());
-    }
-
-    private boolean hasWorkProfile() {
-        UserManager userManager = (UserManager) getSystemService(Context.USER_SERVICE);
-        return userManager.getUserProfiles().size() > 1;
-    }
-
-    private void launchWorkProfileDelayed() {
-
-            LauncherApps launcherApps = (LauncherApps) getSystemService(Context.LAUNCHER_APPS_SERVICE);
-            UserManager userManager = (UserManager) getSystemService(Context.USER_SERVICE);
-            
-            if (launcherApps != null && userManager != null) {
-                List<UserHandle> profiles = userManager.getUserProfiles();
-                for (UserHandle profile : profiles) {
-                   if (userManager.getSerialNumberForUser(profile) != 0) {
-                        launcherApps.startMainActivity(
-                            new ComponentName(getPackageName(), DestroyActivity.class.getName()), 
-                            profile, null, null
-                        );
-                        
-                        finishAndRemoveTask();
-                        break;
-                    }
-                }
-            }
-        }  
+		
+		
 }
